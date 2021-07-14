1e3c420fa74d95cea7598cdfc231d2f76161c432
jbacardit-uz <jbacardit@userzoom.com>
2021-07-14 23:05:06 +0200

    ADD event hub reading dotnet

---
+++ dotnet/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Azure.Storage.Blobs;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+
+/*
+
+More examples here: 
+
+https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/eventhub/Azure.Messaging.EventHubs/samples/Sample05_ReadingEvents.md
+
+*/
+
+namespace examples_dotnet
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connectionString = "...";
+            var eventHubName = "...";
+            var consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+
+            var consumer = new EventHubConsumerClient(
+                consumerGroup,
+                connectionString,
+                eventHubName
+            );
+
+            try
+            {
+                CancellationTokenSource cancellationSource = new CancellationTokenSource();
+                cancellationSource.CancelAfter(TimeSpan.FromSeconds(45));
+
+                int eventsRead = 0;
+                int maximumEvents = 3;
+
+                await foreach (PartitionEvent partitionEvent in consumer.ReadEventsAsync(cancellationSource.Token))
+                {
+                    string readFromPartition = partitionEvent.Partition.PartitionId;
+                    byte[] eventBodyBytes = partitionEvent.Data.EventBody.ToArray();
+                    
+                    string result = System.Text.Encoding.UTF8.GetString(eventBodyBytes);
+
+                    Console.WriteLine($"Read event of length { result } from { readFromPartition }");
+                    eventsRead++;
+
+                    /* if (eventsRead >= maximumEvents)
+                    {
+                        break;
+                    } */
+                }
+            }
+            catch (TaskCanceledException)
+            {
+                // This is expected if the cancellation token is
+                // signaled.
+            }
+            finally
+            {
+                await consumer.CloseAsync();
+            }
+        }
+    }
+}
--- nodejs/eventhub/index.js
+++ nodejs/eventhub/index.js
@@ -1,63 +1,51 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT Licence.
-
-/*
-  This sample demonstrates how to use the EventHubConsumerClient to process events from all partitions
-  of a consumer group in an Event Hubs instance.
-  If your Event Hub instance doesn't have any events, then please run "sendEvents.ts" sample
-  to populate it before running this sample.
-  For an example that uses checkpointing, see the sample in the eventhubs-checkpointstore-blob package
-  on GitHub at the following link:
-  https://github.com/Azure/azure-sdk-for-js/blob/master/sdk/eventhub/eventhubs-checkpointstore-blob/samples/javascript/receiveEventsUsingCheckpointStore.js
-  Note: If you are using version 2.1.0 or lower of @azure/event-hubs library, then please use the samples at
-  https://github.com/Azure/azure-sdk-for-js/tree/%40azure/event-hubs_2.1.0/sdk/eventhub/event-hubs/samples instead.
-*/
+//https://docs.microsoft.com/en-us/javascript/api/overview/azure/event-hubs-readme?view=azure-node-latest#consume-events-from-an-event-hub
 
 const { EventHubConsumerClient, earliestEventPosition } = require("@azure/event-hubs");
 
-// Load the .env file if it exists
-require("dotenv").config();
-
-const connectionString = process.env["EVENTHUB_CONNECTION_STRING"] || "";
-const eventHubName = process.env["EVENTHUB_NAME"] || "";
-const consumerGroup = process.env["CONSUMER_GROUP_NAME"] || "";
-
 async function main() {
-  console.log(`Running receiveEvents sample`);
 
-  const consumerClient = new EventHubConsumerClient(consumerGroup, connectionString, eventHubName);
+  require("dotenv").config();
 
-  const subscription = consumerClient.subscribe(
+  const connectionString = process.env["EVENTHUB_CONNECTION_STRING"] || "";
+  const eventHubName = process.env["EVENTHUB_NAME"] || "";
+  const consumerGroup = process.env["CONSUMER_GROUP_NAME"] || "";
+
+  const client = new EventHubConsumerClient(
+    consumerGroup,
+    connectionString,
+    eventHubName
+  );
+  const partitionIds = await client.getPartitionIds();
+
+  // In this sample, we use the position of earliest available event to start from
+  // Other common options to configure would be `maxBatchSize` and `maxWaitTimeInSeconds`
+  const subscriptionOptions = {
+    startPosition: earliestEventPosition
+  };
+
+  const subscription = client.subscribe(
+    partitionIds[0],
     {
-      // The callback where you add your code to process incoming events
       processEvents: async (events, context) => {
-        // Note: It is possible for `events` to be an empty array.
-        // This can happen if there were no new events to receive
-        // in the `maxWaitTimeInSeconds`, which is defaulted to
-        // 60 seconds.
-        // The `maxWaitTimeInSeconds` can be changed by setting
-        // it in the `options` passed to `subscribe()`.
         for (const event of events) {
-          console.log(
-            `Received event: '${JSON.stringify(event.body)}' from partition: '${context.partitionId}' and consumer group: '${context.consumerGroup}'`
-          );
-        }
+            console.log(
+              `Received event: '${JSON.stringify(event.body)}' from partition: '${context.partitionId}' and consumer group: '${context.consumerGroup}'`
+            );
+          }
       },
       processError: async (err, context) => {
-        console.log(`Error on partition "${context.partitionId}": ${err}`);
+        // error reporting/handling code here
       }
     },
-    { startPosition: earliestEventPosition }
+    subscriptionOptions
   );
 
-  // Wait for a bit before cleaning up the sample
+  // Wait for a few seconds to receive events before closing
   setTimeout(async () => {
     await subscription.close();
-    await consumerClient.close();
-    console.log(`Exiting receiveEvents sample`);
-  }, 30 * 1000);
+    await client.close();
+    console.log(`Exiting sample`);
+  }, 600 * 1000);
 }
 
-main().catch((error) => {
-  console.error("Error running sample:", error);
-});
+main();
